5028b48e6b8c1cecb7b7567e8bfed30382eb38fd
Sanja <gamesg274@gmail.com>
2022-06-28 20:26:29 +0200

    Created project structure

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+1. Create 16x16 grid of square divs
+2. Add hover to div squares (black and white)
+3. Add button asking for the number of squares per side for the new grid.
+Bonus: Add random rgb hover effect
+*/
+
+document.getElementById("gridSizeInput").addEventListener("change", () => {
+  console.log(document.getElementById("gridSizeInput").value)
+})
+
